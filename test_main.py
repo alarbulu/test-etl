f98505b0f2a07790288b9667e12c5f0984c1c86a
Alice Wong <alice.wong@thedatalab.org>
2025-08-08 23:28:42 +0100

    Simplify `repos_pages` fixture :recycle:

--- test_main.py
+++ test_main.py
@@ -5,13 +5,9 @@ import types
 
 @pytest.fixture
 def repos_pages():
-    def _repos_pages():
-        page_1 = [{"name": "active_repo"}, {"name": "abandoned_repo"}]
-        page_2 = [{"name": "empty_repo"}]
-        yield page_1
-        yield page_2
-
-    return _repos_pages()
+    page_1 = [{"name": "active_repo"}, {"name": "abandoned_repo"}]
+    page_2 = [{"name": "empty_repo"}]
+    return iter([page_1, page_2])
 
 
 def test_get_repo_names(repos_pages):
