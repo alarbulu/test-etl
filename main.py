b40590af3313c19dc556b51a22ccbe84e9cee1ce
Alice Wong <alice.wong@thedatalab.org>
2025-08-08 21:47:51 +0100

    Add `test_get_workflow_runs` :apple:

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ def get_repo_names(repos_pages):
     return (repo["name"] for page in repos_pages for repo in page)
 
 
+def get_workflow_runs(workflow_runs_pages):
+    pass
+
+
 def main():
     pass
 
--- test_main.py
+++ test_main.py
@@ -4,13 +4,56 @@ import types
 
 
 @pytest.fixture
+def workflow_run_template():
+    return {
+        "id": None,  # Placeholder
+        "name": "My Workflow",
+        "head_sha": 12345678,
+        "status": "pending",
+        "conclusion": None,
+        "created_at": "2025-01-01T00:00:00Z",
+        "updated_at": "2025-01-01T00:00:00Z",
+        "run_started_at": "2025-01-01T00:00:00Z",
+        "repository": {
+            "name": None  # Placeholder
+        },
+    }
+
+
+@pytest.fixture
 def repos_pages():
     page_1 = [{"name": "active_repo"}, {"name": "abandoned_repo"}]
     page_2 = [{"name": "empty_repo"}]
     return iter([page_1, page_2])
 
 
+@pytest.fixture
+def active_repo_workflow_run_pages(workflow_run_template):
+    template = workflow_run_template | {"repository": {"name": "active_repo"}}
+    run_1 = template | {"id": 1}
+    run_2 = template | {"id": 2}
+    run_3 = template | {"id": 3}
+
+    pages = [
+        {"total_count": 3, "workflow_runs": [run_1, run_2]},
+        {"total_count": 3, "workflow_runs": [run_3]},
+    ]
+    return iter(pages)
+
+
 def test_get_repo_names(repos_pages):
     repo_names = main.get_repo_names(repos_pages)
     assert isinstance(repo_names, types.GeneratorType)
     assert list(repo_names) == ["active_repo", "abandoned_repo", "empty_repo"]
+
+
+def test_get_workflow_runs(active_repo_workflow_run_pages):
+    workflow_runs = main.get_workflow_runs(active_repo_workflow_run_pages)
+    assert isinstance(workflow_runs, types.GeneratorType)
+    runs_list = list(workflow_runs)
+    assert len(runs_list) == 3
+    run_1, run_2, run_3 = runs_list
+    assert run_1["repository"]["name"] == "active_repo"
+    assert run_1["id"] == 1
+    assert run_2["id"] == 2
+    assert run_3["id"] == 3
