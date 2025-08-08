17c88cfd18467f4326e1dac618f66b200423fd52
Alice Wong <alice.wong@thedatalab.org>
2025-08-08 22:01:44 +0100

    Implement JSON decoding :green_apple:

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 def get_repo_names(repos_pages):
-    return (repo["name"] for page in repos_pages for repo in page)
+    decoded_pages = (page.json() for page in repos_pages)
+    return (repo["name"] for page in decoded_pages for repo in page)
 
 
 def get_workflow_runs(workflow_runs_pages):
-    return (run for page in workflow_runs_pages for run in page["workflow_runs"])
+    decoded_pages = (page.json() for page in workflow_runs_pages)
+    return (run for page in decoded_pages for run in page["workflow_runs"])
 
 
 def main():
