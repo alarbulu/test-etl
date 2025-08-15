ac1a609199dafac26c7f31edc54c742ce571f32b
Alice Wong <alice.wong@thedatalab.org>
2025-08-15 18:59:47 +0100

    Write user access token to `.env`
    Still problems - the current terminal's environment variables won't be
    updated if I just write to the `.env` file. I need to source the env
    file in the current terminal session.

--- auth.py
+++ auth.py
@@ -1,6 +1,7 @@
 import collections
 import os
 import time
+import datetime
 from urllib.parse import parse_qs
 
 import requests
@@ -9,65 +10,107 @@ import requests
 class GitHubAPISession(requests.Session):
     def __init__(self, token=None):
         super().__init__()
-        token = self._get_token()
+        token = self.get_token()
         self.headers.update({"Authorization": f"Bearer {token}"})
 
-    def _get_token(self):
-        try:
-            return os.environ["GITHUB_WORKFLOW_RUNS_TOKEN"]
-        except KeyError:
-            return get_token(os.environ["GITHUB_APP_CLIENT_ID"])
+    def get_token(self):
+        token = os.environ.get("GITHUB_WORKFLOW_RUNS_TOKEN")
+        expiry = os.environ.get("GITHUB_WORKFLOW_RUNS_TOKEN_EXPIRY")
+        if token and not expiry:
+            # This is a PAT provided by the user.
+            return token
+        elif (
+            token
+            and expiry
+            and datetime.datetime.fromisoformat(expiry) > datetime.datetime.now()
+        ):
+            return token
+        else:
+            return self._refresh_github_app_user_access_token()
+
+    def _refresh_github_app_user_access_token(self):
+        # Let the KeyError get raised if there is no client ID,
+        # as either this should be set, or the user should provide their own token.
+        app_auth = GitHubAppAuthentication(os.environ["GITHUB_APP_CLIENT_ID"])
+        token, expiry = app_auth.get_user_access_token()
+        replace_value_in_dotenv("GITHUB_WORKFLOW_RUNS_TOKEN", token)
+        replace_value_in_dotenv("GITHUB_WORKFLOW_RUNS_TOKEN_EXPIRY", expiry.isoformat())
+        return token
 
 
-LoginResponse = collections.namedtuple(
-    "LoginResponse",
-    [
-        "device_code",
-        "user_code",
-        "verification_uri",
-        "expires_in",
-        "interval",
-    ],
-)
+def replace_value_in_dotenv(key, value):
+    lines = []
+    for line in open(".env", "r").readlines():
+        if not line.startswith(f"{key}="):
+            lines.append(line)
+    lines.append(f"{key}={value}\n")
+    with open(".env", "w") as f:
+        f.writelines(lines)
 
 
-def post_login_request(app_client_id):
-    response = requests.post(
-        "https://github.com/login/device/code",
-        data={
-            "client_id": app_client_id,
-        },
+class GitHubAppAuthentication:
+    LoginResponse = collections.namedtuple(
+        "LoginResponse",
+        [
+            "device_code",
+            "user_code",
+            "verification_uri",
+            "expires_in",
+            "interval",
+        ],
     )
-    response.raise_for_status()
-    response_data = parse_qs(response.text)
-    return LoginResponse(
-        device_code=response_data["device_code"][0],
-        user_code=response_data["user_code"][0],
-        verification_uri=response_data["verification_uri"][0],
-        expires_in=int(response_data["expires_in"][0]),
-        interval=int(response_data["interval"][0]),
-    )
 
+    def __init__(self, app_client_id):
+        self.app_client_id = app_client_id
 
-def get_token(app_client_id):
-    login = post_login_request(app_client_id)
-    timeout_time = time.time() + login.expires_in
+    def post_login_request(self):
+        response = requests.post(
+            "https://github.com/login/device/code",
+            data={
+                "client_id": self.app_client_id,
+            },
+        )
+        response.raise_for_status()
+        response_data = parse_qs(response.text)
+        return self.LoginResponse(
+            device_code=response_data["device_code"][0],
+            user_code=response_data["user_code"][0],
+            verification_uri=response_data["verification_uri"][0],
+            expires_in=int(response_data["expires_in"][0]),
+            interval=int(response_data["interval"][0]),
+        )
 
-    input(
-        f"Please go to {login.verification_uri} and enter the code: {login.user_code}\n"
-        f"This code will expire in {login.expires_in} seconds.\n"
-        f"Press any key to continue after you have authorized the app."
-    )
-    while time.time() < timeout_time:
+    def post_oauth_request(self, device_code):
         response = requests.post(
             "https://github.com/login/oauth/access_token",
             data={
-                "client_id": app_client_id,
-                "device_code": login.device_code,
+                "client_id": self.app_client_id,
+                "device_code": device_code,
                 "grant_type": "urn:ietf:params:oauth:grant-type:device_code",
             },
         )
-        if response.status_code == 200:
-            (access_token,) = parse_qs(response.text)["access_token"]
-            return access_token
-        time.sleep(login.interval + 1)
+
+        return response
+
+    def ask_user_to_authorize_device(self, login):
+        input(
+            f"Please go to {login.verification_uri} and enter the code: {login.user_code}\n"
+            f"This code will expire in {login.expires_in // 60} minutes {login.expires_in % 60} seconds.\n"
+            f"Press any key to continue after you have authorized the app."
+        )
+
+    def get_user_access_token(self):
+        login = self.post_login_request()
+        timeout_time = time.time() + login.expires_in
+        self.ask_user_to_authorize_device(login)
+        while time.time() < timeout_time:
+            response = self.post_oauth_request(login.device_code)
+            if response.status_code == 200:
+                qs = parse_qs(response.text)
+                access_token = qs["access_token"][0]
+                expires_in = int(qs["expires_in"][0])
+                return (
+                    access_token,
+                    datetime.datetime.now() + datetime.timedelta(seconds=expires_in),
+                )
+            time.sleep(login.interval + 1)
--- test_auth.py
+++ test_auth.py
@@ -2,7 +2,9 @@ import auth
 
 
 def test_github_api_session_init(monkeypatch):
+    # Give a PAT without expiry date
     monkeypatch.setenv("GITHUB_WORKFLOW_RUNS_TOKEN", "test_token")
+    monkeypatch.delenv("GITHUB_WORKFLOW_RUNS_TOKEN_EXPIRY")
     session = auth.GitHubAPISession()
 
     assert session.headers["Authorization"] == "Bearer test_token"
