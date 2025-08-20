d2481a675caa203549837091e46eb1ffde1a5891
Alice Wong <alice.wong@thedatalab.org>
2025-08-20 16:58:48 +0100

    Check `assert False` fails CI

--- main.py
+++ main.py
@@ -180,3 +180,4 @@ if __name__ == "__main__":
     with GitHubAPISession() as session:
         session.params.update({"per_page": 100, "format": "json"})
         main(session, pathlib.Path("data"))
+    assert False
